a13da99109685596f1736f6d54bc26f9366425cd
luciayin9944 <luxiayin94@gmail.com>
2025-07-31 14:12:00 -0700

    implement signup/login backend

--- server/app.py
+++ server/app.py
@@ -4,6 +4,9 @@ from flask_restful import Api, Resource
 from config import db, bcrypt, jwt, Config
 from flask_cors import CORS
 from models import *  
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+
 
 def create_app():
     app = Flask(__name__)
@@ -16,4 +19,62 @@ def create_app():
     jwt.init_app(app)
     api = Api(app)
 
+    # API resources
+    api.add_resource(Signup, './signup')
+    api.add_resource(WhoAmI, './me')
+    api.add_resource(Login, './login')
+
     return app
+
+
+class Signup(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
+        if not username or not email or not password:
+            return {'errors': ['Missing usernamer or email or password']}, 400
+        
+        user = User(username=username)
+        user.password_hash = password
+        user.email = email
+
+        try:
+            db.session.add(user)
+            db.session.commit()
+            access_token = create_access_token(identity=str(user.id))
+            response = make_response(jsonify(token=access_token, user=UserSchema().dump(user)), 200)
+            return response
+        except IntegrityError:
+            db.session.rollback()
+            return {'errors': ['Username or Email already exists']}, 422
+        
+
+
+class WhoAmI(Resource):
+    @jwt_required()
+    def get(self):
+        user_id = get_jwt_identity()
+        user = User.query.get(user_id)
+        if user:
+            return UserSchema().dump(user), 200
+        return {'errors': ['User not found']}, 404
+    
+
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        email = data.get('email')
+        password = data.get('password')
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.autherticate(password):
+            access_token = create_access_token(identity=str(user.id))
+            response = make_response(jsonify(token=access_token, user=UserSchema().dump(user)), 200)
+            return response
+        return {'errors': ['Invalid email or password']}, 401
+
+
+
